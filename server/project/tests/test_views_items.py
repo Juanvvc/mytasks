e9b558d98616de627941747fc60e4540a96ed5bd
Juanvi Vera <juanvvc@gmail.com>
2018-09-30 17:27:54 +0200

    Server: separated item control

---
+++ server/project/tests/test_views_items.py
@@ -0,0 +1,157 @@
+import unittest
+import flask
+import flask_testing
+import project.model
+import project.views
+from project.tests import HTTPHelper
+
+
+class TestItemsView(flask_testing.TestCase):
+    def create_app(self):
+        app = flask.Flask(__name__)
+        app.config.from_object('project.server.config.TestingConfig')
+        project.model.configure_model(app)
+        project.views.register(app)
+
+        @app.errorhandler(400)
+        @app.errorhandler(404)
+        @app.errorhandler(401)
+        @app.errorhandler(500)
+        def error_handler(error):
+            return flask.make_response(flask.jsonify({'error_message': str(error), 'status': error.code}))
+
+        return app
+
+    def setUp(self):
+        self.user = project.model.create_user('USER1', 'PASSWORD1')
+        self.user2 = project.model.create_user('USER2', 'PASSWORD2')
+        self.group = self.user.create_child({'name': 'GROUP1', 'private': False})
+        self.group2 = self.user.create_child({'name': 'GROUP1', 'private': True})
+        self.checklist1 = self.group.create_child({'name': 'CHECKLIST1'})
+        self.checklist2 = self.group2.create_child({'name': 'CHECKLIST2'})
+        self.item1 = self.checklist1.create_child({'name': 'ITEM1'})
+        self.item2 = self.checklist1.create_child({'name': 'ITEM2'})
+        self.item3 = self.checklist2.create_child({'name': 'ITEM3'})
+
+        project.model.create_user('USER2', 'PASSWORD2')
+
+    def tearDown(self):
+        project.model.db.command('dropDatabase')
+
+    def test_availableitems(self):
+        with self.client:
+            url = flask.url_for('checklists.info', _id=str(self.checklist1.id()))
+            http = HTTPHelper(self.client, ['USER1', 'PASSWORD1'])
+            data = http.get(url)
+            items = data.get('items', [])
+            self.assertTrue(type(items) == list)
+            self.assertEqual(len(items), 2)
+            self.assertTrue(items[0].get('name', '') in ('ITEM1', 'ITEM2'))
+
+    def test_oneitem(self):
+        with self.client:
+            http = HTTPHelper(self.client, ['USER1', 'PASSWORD1'])
+
+            # USER1 can access item1
+            url = flask.url_for('items.info', _id=str(self.item1.id()))
+            data = http.get(url)
+            self.assertFalse('error_message' in data)
+            self.assertEqual(data.get('name', None), 'ITEM1')
+            # USER2 can access to item1, since it is in a private group
+            data = http.get(url, auth=['USER2', 'PASSWORD2'])
+            self.assertFalse('error_message' in data)
+            self.assertEqual(data.get('name', None), 'ITEM1')
+
+            # USER1 can access item3
+            url = flask.url_for('items.info', _id=str(self.item3.id()))
+            data = http.get(url)
+            self.assertFalse('error_message' in data)
+            self.assertEqual(data.get('name', None), 'ITEM3')
+            # USER2 cannot access to item3, since it is in a private group
+            data = http.get(url, auth=['USER2', 'PASSWORD2'])
+            self.assertEqual(data.get('name', None), None)
+            self.assertTrue('error_message' in data)
+            self.assertEqual(data.get('status', 0), 401)
+
+    def test_newitem(self):
+        """ Test creation of an item, and its errors """
+        with self.client:
+            url = flask.url_for('items.new')
+            http = HTTPHelper(self.client, ['USER1', 'PASSWORD1'])
+
+            # try to create an item without information
+            data = http.post(url)
+            self.assertEqual(data.get('status', 0), 400)
+
+            # create an item
+            new_info = dict(name='NEWNAME', _parentid=str(self.checklist1.id()))
+            data = http.post(url, data=new_info)
+            self.assertFalse('error_message' in data)
+            self.assertEqual(data.get('name', None), 'NEWNAME')
+            self.assertEqual(data.get('_parentid', None), str(self.checklist1.id()))
+
+    def test_updateitem(self):
+        """ Test to update an item, and its errors """
+        with self.client:
+            url = flask.url_for('items.info', _id=str(self.item3.id()))
+            http = HTTPHelper(self.client, ['USER1', 'PASSWORD1'])
+
+            # try to update an item without information
+            data = http.put(url)
+            self.assertTrue('error_message' in data)
+            self.assertEqual(data['status'], 400)
+            # get item3, check the old name
+            data = http.get(url)
+            self.assertEqual(data.get('name', None), 'ITEM3')
+            # update item3
+            data = http.post(url, data=dict(name='NEWNAME'))
+            self.assertTrue('error_message' not in data)
+            # get item3, check the new name
+            data = http.get(url)
+            self.assertEqual(data.get('name', None), 'NEWNAME')
+            # get the information of the checklist, test the name is new one
+            data = http.get(flask.url_for('checklists.info', _id=data['_parentid']))
+            self.assertEqual(data['items'][0].get('name', None), 'NEWNAME')
+
+            # USER2 can get but not update an item in a public group owned by USER1
+            url = flask.url_for('items.info', _id=str(self.item1.id()))
+            data = http.get(url, auth=["USER2", "PASSWORD2"])
+            self.assertFalse('error_message' in data)
+            data = http.put(url, data=dict(name='NEWNAME1'), auth=["USER2", "PASSWORD2"])
+            self.assertTrue('error_message' in data)
+            self.assertEqual(data['status'], 401)
+
+    def test_deleteitem(self):
+        """ Test deleting an item, and its errors """
+        with self.client:
+            url = flask.url_for('items.info', _id=str(self.item3.id()))
+            http = HTTPHelper(self.client, ['USER1', 'PASSWORD1'])
+
+            # check USER2: he cannot delete the item
+            data = http.delete(url, auth=['USER2', 'PASSWORD2'])
+            self.assertEqual(data.get('status', 0), 401)
+
+            # check item3 exists
+            data = http.get(url)
+            self.assertEqual(data.get('name', None), 'ITEM3')
+            # delete item3
+            data = http.delete(url)
+            self.assertEqual(data.get('status', 0), 200)
+            # check it does not exist
+            data = http.get(url)
+            self.assertEqual(data.get('status', 0), 404)
+            data = http.delete(url)
+            self.assertEqual(data.get('status', 0), 404)
+
+            # non existing checklist
+            url = flask.url_for('checklists.info', _id='XXX')
+            data = http.delete(url)
+            self.assertTrue('error_message' in data)
+            self.assertEqual(data.get('status', 0), 404)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ server/project/views/items.py
@@ -0,0 +1,91 @@
+import flask
+import project.model as model
+
+
+def get_blueprint(auth=None):
+    blueprint = flask.Blueprint('items', __name__)
+    blueprint.add_url_rule('/items/', view_func=auth.login_required(new_item), methods=['POST', 'PUT'], endpoint='new')
+    blueprint.add_url_rule('/items/<_id>', view_func=auth.login_required(single_item), methods=['GET'], endpoint='info')
+    blueprint.add_url_rule('/items/<_id>', view_func=auth.login_required(update_item), methods=['POST', 'PUT'], endpoint='update')
+    blueprint.add_url_rule('/items/<_id>', view_func=auth.login_required(delete_item), methods=['DELETE'], endpoint='delete')
+    return blueprint
+
+
+def new_item():
+    new_info = flask.request.json
+    if not new_info or new_info is None:
+        flask.abort(400, 'No information provided')
+
+    # check the properties
+    name = new_info.get('name', None)
+    if name is None:
+        flask.abort(400, 'An item needs a name')
+
+    # check the checklist
+    checklist_id = new_info.get('_parentid', None)
+    if checklist_id is None:
+        flask.abort(400, 'A checklists needs a name')
+    checklist = model.search_element(model.Checklist, checklist_id)
+    if checklist is None:
+        flask.abort(404, 'Group not found')
+
+    # create the checklist
+    item = checklist.create_child(new_info)
+    if(checklist.save()):
+        return single_item(item.id())
+    else:
+        flask.abort(500, 'Error while saving new item')
+
+
+def single_item(_id):
+    item = model.search_element(model.Item, _id)
+    # check the list exists
+    if item is None:
+        flask.abort(404, 'Item not found')
+    # check the user is allowed to access the list
+    if not item.visible_by(flask.g.user_id):
+        flask.abort(401, 'Not allowed to access this item')
+
+    info = item.sane_info()
+    info['uri'] = flask.url_for('items.info', _id=_id, _external=True)
+    return flask.jsonify(info)
+
+
+def update_item(_id):
+    item = model.search_element(model.Item, _id)
+    # check the checklist exists and it is editable by the current user
+    if item is None:
+        flask.abort(404, 'Item not found')
+    if not item.editable_by(flask.g.user_id):
+        flask.abort(401, 'Your are not allowed to edit this item')
+
+    new_info = flask.request.json
+    if not new_info or new_info is None:
+        flask.abort(400, 'No information')
+    item.info.update(new_info)
+    if(item.save()):
+        return single_item(_id)
+    else:
+        flask.abort(500, 'Error while saving item')
+
+
+def delete_item(_id):
+    item = model.search_element(model.Item, _id)
+    # check the checklist exists and it is editable by the current user
+    if item is None:
+        flask.abort(404, 'Item not found')
+    if not item.editable_by(flask.g.user_id):
+        flask.abort(401, 'You are not allowed to edit this item')
+
+    # delete the item from the checklist
+    if '_parentid' in item.info:
+        checklist = model.search_element(model.Checklist, item.info['_parentid'])
+        if not checklist.delete_child(_id):
+            flask.current_app.logger.error('Item %s not found in checklist', _id)
+    else:
+        flask.current_app.logger.error('Item %s doesn\'t belong to a checklist', _id)
+
+    if item.delete():
+        return flask.jsonify({'status': 200, 'message': 'Item {} deleted'.format(_id)})
+    else:
+        flask.abort(500, 'Error while deleting checklist')
